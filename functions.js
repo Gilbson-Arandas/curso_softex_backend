fb940c350ad5d819252569bbe89f3c754b704bae
Gilbson Arandas <109983801+Gilbson-Arandas@users.noreply.github.com>
2022-08-25 02:16:02 -0300

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,18 @@
+// Primeira Função
+firstFunction = function () {
+    console.log('Função sem retorno');
+}
+
+// Segunda Função
+secondFunction = function () {
+    return 'Alis Volat Propriis!'
+}
+
+// Terceira Função
+thirdFunction = (value) => {
+    return value;
+}
+
+firstFunction();
+console.log(secondFunction());
+console.log(thirdFunction('Utilius tarde quam nunquam'));
